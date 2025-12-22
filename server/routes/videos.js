4c5bc88aabb3ce796c5e910c0c103a513baccce6
Siddhi Chavan <siddhichavan073@gmail.com>
2025-12-22 19:34:13 +0530

    videos.js modified

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -6,26 +6,26 @@ const router = express.Router()
 
 
 router.get('/all_videos', (req, res) => {
-    const course_id = req.body
+    const {course_id} = req.body
     const sql = `SELECT * FROM videos WHERE course_id = ?`
     pool.query(sql, [course_id], (error, data) => {
-        res.send(createResult(error, data))
-        // console.log(data);
+        res.send(result.createResult(error, data))
+        //console.log(data);
     })
 })
 
 router.post('/add', (req, res) => {
     const {course_id, title, youtube_url, description} = req.body
-    const sql = `INSERT INTO VIDEOS(course_id, title, youtube_url, description) VALUES (?,?,?,?)`
+    const sql = `INSERT INTO videos (course_id, title, youtube_url, description) VALUES (?,?,?,?)`
     pool.query(sql , [course_id, title, youtube_url, description], (error , data) => {
-        res.send(createResult(error, data))
+        res.send(result.createResult(error, data))
     })
 
 })
 
 //request parameter(write parameters in the URL)
 router.put('/update/:video_id', (req, res) => {
-    const video_id = req.params.video_id
+    const {video_id} = req.params
     const {course_id, title, youtube_url, description} = req.body
     const sql = `UPDATE videos SET course_id = ?, title = ?, youtube_url = ?, description =? WHERE video_id = ?`
     pool.query(sql, [course_id, title, youtube_url, description, video_id], (error, data) => {
@@ -35,7 +35,7 @@ router.put('/update/:video_id', (req, res) => {
 
 //delete video on video_id
 router.delete('/delete/:video_id', (req, res) => {
-    const video_id = req.params.video_id
+    const {video_id} = req.params
     const sql = `DELETE FROM videos WHERE video_id = ?`
     pool.query(sql , [video_id], (error, data) => {
         res.send(result.createResult(error, data))
