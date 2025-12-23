50204e402744195249177d032152ea784c5e63cf
Namrata Kshirsagar <kshirsagarnamrata24@gmail.com>
2025-12-23 08:23:22 +0530

    courses.js added by namrata

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const coursesRouter=express.Router()
 
 const app = express()
+
+app.use('/course',coursesRouter)
 
 app.listen(4000, 'localhost', () => {
     console.log("Server started at port 4000")
