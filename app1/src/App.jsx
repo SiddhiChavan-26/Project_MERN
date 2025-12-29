8e580fc7eca9ee609025441a4c8cbc7efdcab826
Ankita Mane <maneankita831@gmail.com>
2025-12-30 00:48:09 +0530

    Student pages added by Ankita

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from "react-router"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import { ToastContainer } from 'react-toastify'
+import RegisterCourse from "./pages/RegisterCourse"
+import Mycourses from "./pages/Mycourses"
 
 
 // functional components
@@ -11,6 +13,8 @@ function App() {
         <Routes>          
           <Route path="/*" element={<Home /> } />
           <Route path="/about" element={<About /> } />
+          <Route path ="/registercourse" element={<RegisterCourse/>}/>
+          <Route path="/mycourses" element={< Mycourses />}/>
         </Routes>
       
       <ToastContainer />
